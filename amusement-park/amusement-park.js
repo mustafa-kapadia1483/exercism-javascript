64c013155b3207bc32c9d86bced9b3bae1b4f461
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-05-06 22:48:03 +0530

    iteration 1

---
+++ amusement-park/amusement-park.js
@@ -0,0 +1,83 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  const visitorObject = {
+    name,
+    age,
+    ticketId,
+  };
+
+  return visitorObject;
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  visitor.ticketId = null;
+  return visitor;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  let ticketStatus = "unknown ticket id";
+  for (const ticket in tickets) {
+    if (ticket === ticketId) {
+      const ticketValue = tickets[ticket];
+      ticketStatus =
+        ticketValue === null ? "not sold" : `sold to ${ticketValue}`;
+      break;
+    }
+  }
+
+  return ticketStatus;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  let ticketStatus = "invalid ticket !!!";
+
+  for (const ticket in tickets) {
+    if (ticket === ticketId && tickets[ticket] !== null) {
+      ticketStatus = tickets[ticket];
+      break;
+    }
+  }
+
+  return ticketStatus;
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+  return visitor.gtc?.version;
+}
