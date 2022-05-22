967dded42b4d13512b070a195950b53041714d60
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-05-22 20:48:27 +0530

    iteration 1

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = ([color1, color2]) => {
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  return colorCodes[color1] * 10 + colorCodes[color2];
+};
