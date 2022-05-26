2f339d0a168fe5c9dddc3f8ea82649bc060d5291
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-05-26 20:53:54 +0530

    iteration 2

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -5,27 +5,20 @@
 
 const EARTH_SECONDS_IN_A_YEAR = 31557600;
 
-export const age = (planet, seconds) => {
-  switch (planet) {
-    case "mercury":
-      return calculate(0.2408467);
-    case "venus":
-      return calculate(0.61519726);
-    case "mars":
-      return calculate(1.8808158);
-    case "jupiter":
-      return calculate(11.862615);
-    case "saturn":
-      return calculate(29.447498);
-    case "uranus":
-      return calculate(84.016846);
-    case "neptune":
-      return calculate(164.79132);
-    case "earth":
-      return calculate(1);
+const ORBITAL_PERIODS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
 
-      function calculate(yearsToEarth) {
-        return +(seconds / (EARTH_SECONDS_IN_A_YEAR * yearsToEarth)).toFixed(2);
-      }
-  }
+export const age = (planet, seconds) => {
+  return +(
+    seconds /
+    (EARTH_SECONDS_IN_A_YEAR * ORBITAL_PERIODS[planet])
+  ).toFixed(2);
 };
