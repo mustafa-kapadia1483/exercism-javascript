bd9475a222859ee2afafec9ca8d4b61a3b70f8e9
Mustafa Kapadia <mustafakapadia76@gmail.com>
2024-04-09 17:52:21 +0530

    iteration 2

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,10 +1,7 @@
 export const isPangram = text => {
-  if (typeof text !== "string") return false;
-
   text = text.toLowerCase();
-  for (const letter of "abcdefghijklmnopqrstuvwxyz") {
-    if (!text.includes(letter)) return false;
-  }
 
-  return true;
+  return "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .every(letter => text.includes(letter));
 };
