44c40927f40beae03028f128c05afa80dbb304ca
Mustafa Kapadia <mustafakapadia76@gmail.com>
2024-04-09 21:10:50 +0530

    iteration 2

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,13 +1,1 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const reverseString = text => {
-  let reversedText = "";
-  for (let i = text.length - 1; i >= 0; i--) {
-    reversedText += text[i];
-  }
-
-  return reversedText;
-};
+export const reverseString = text => Array.from(text).reverse().join("");
